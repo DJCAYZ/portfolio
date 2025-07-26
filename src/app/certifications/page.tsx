0e4ed6afb217355739786bbde7fceb632c72aba7
DJCAYZ <charlesaaron07@gmail.com>
2025-07-27 04:27:49 +0800

    Initial Project Setup

---
+++ src/app/certifications/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Certifications() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-2 min-h-screen">
+      <h2 className="text-2xl font-bold">Certifications</h2>
+      <h3 className="text-xl">Soon.</h3>
+      <Button asChild>
+        <Link href="/">Go Back</Link>
+      </Button>
+    </div>
+  )
+}
