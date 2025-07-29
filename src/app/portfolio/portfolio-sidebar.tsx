0955ae076c57e7e0ca8c963841aeb32feb96f05e
DJCAYZ <charlesaaron07@gmail.com>
2025-07-29 22:38:36 +0800

    fix portfolio sidebar rendering bullets on edge

--- src/app/portfolio/portfolio-sidebar.tsx
+++ src/app/portfolio/portfolio-sidebar.tsx
@@ -1,4 +1,4 @@
-import { Sidebar, SidebarContent, SidebarHeader, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem } from "@/components/ui/sidebar";
+import { Sidebar, SidebarContent, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem } from "@/components/ui/sidebar";
 import { courses } from "@/lib/data";
 import { toProperCase } from "@/lib/utils";
 import { ArrowLeft } from "lucide-react";
@@ -20,45 +20,47 @@ export function PortfolioSidebar() {
 
   return (
     <Sidebar className="">
-      <SidebarHeader>
-        <SidebarMenuItem>
-          <SidebarMenuButton asChild>
-            <Link href="/">
-              <ArrowLeft />
-              Home
-            </Link>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
-      </SidebarHeader>
-      <SidebarContent className="p-2">
-        <SidebarMenuItem>
-          <SidebarMenuButton asChild>
-            <Link href='/portfolio'>Portfolio Index</Link>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
-        <SidebarMenuItem>
-          <SidebarMenuButton asChild>
-            <Link href='/portfolio/submission'>All Submissions</Link>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
-        {items.map((item, index) => (
-          <SidebarMenuItem key={index}>
+      <SidebarMenu>
+        <SidebarHeader>
+          <SidebarMenuItem>
             <SidebarMenuButton asChild>
-              <Link href={item.url}>{item.title}</Link>
+              <Link href="/">
+                <ArrowLeft />
+                Home
+              </Link>
             </SidebarMenuButton>
-
-            <SidebarMenuSub>
-              {item.subItems?.map((subItem, sIndex) => (
-                <SidebarMenuSubItem key={sIndex}>
-                  <SidebarMenuSubButton asChild>
-                    <Link href={subItem.url}>{subItem.title}</Link>
-                  </SidebarMenuSubButton>
-                </SidebarMenuSubItem>
-              ))}
-            </SidebarMenuSub>
           </SidebarMenuItem>
-        ))}
-      </SidebarContent>
+        </SidebarHeader>
+        <SidebarContent className="p-2">
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href='/portfolio'>Portfolio Index</Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href='/portfolio/submission'>All Submissions</Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+          {items.map((item, index) => (
+            <SidebarMenuItem key={index}>
+              <SidebarMenuButton asChild>
+                <Link href={item.url}>{item.title}</Link>
+              </SidebarMenuButton>
+
+              <SidebarMenuSub>
+                {item.subItems?.map((subItem, sIndex) => (
+                  <SidebarMenuSubItem key={sIndex}>
+                    <SidebarMenuSubButton asChild>
+                      <Link href={subItem.url}>{subItem.title}</Link>
+                    </SidebarMenuSubButton>
+                  </SidebarMenuSubItem>
+                ))}
+              </SidebarMenuSub>
+            </SidebarMenuItem>
+          ))}
+        </SidebarContent>
+      </SidebarMenu>
     </Sidebar>
   )
 }
