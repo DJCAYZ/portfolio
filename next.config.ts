5fe5b0b7f2832706b0551c858cd0b041913d144b
DJCAYZ <charlesaaron07@gmail.com>
2025-08-01 09:45:29 +0800

    add another address to allowed dev origins

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  "allowedDevOrigins": ['192.168.1.107']
+  "allowedDevOrigins": ['192.168.1.107', '192.168.131.37']
 };
 
 export default nextConfig;
