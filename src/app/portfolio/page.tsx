8f90d126603f6a9746b29581254cff2b4de8fc4d
DJCAYZ <charlesaaron07@gmail.com>
2025-08-04 12:14:01 +0800

    add sidebartrigger to portfolio index page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import { courses } from "@/lib/data";
 import { cn, getColor } from "@/lib/utils";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function PortfolioPage() {
   const items = (Object.keys(courses) as Array<keyof typeof courses>).map(key => ({...courses[key], key}));
   return (
-    <div className="flex flex-col items-center gap-2">
-      <h1 className="text-3xl mb-2">Courses</h1>
+    <div className="flex flex-col gap-2">
+      <div className="flex gap-2 items-center">
+        <SidebarTrigger className="w-8 h-8" />
+        <h1 className="text-3xl mb-2">Courses</h1>
+      </div>
       {items.map((item, index) => (
         <Link
           href={`/portfolio/${item.key}`}
